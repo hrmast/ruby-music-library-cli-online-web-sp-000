9d17ee4949032bb9913b6568d816d7ce8abd8f1f
Heath Mast <hrmast@gmail.com>
2020-09-25 00:20:04 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -101,7 +101,8 @@ class MusicLibraryController
         Song.all.sort {|a, b| a.name <=> b.name}.uniq.each do |song|
           choice << "Playing #{song.name} by #{song.artist.name}"
         end
-        # binding.pry
-
+         binding.pry
+        puts choice[input - 1]
+      end
     end
 end
